bbece95be0eb519249e0c012035408efc99366fb
editprior <146826298+editprior@users.noreply.github.com>
2024-03-12 14:05:33 +0100

    Update SDG_app.py

--- SDG_app.py
+++ SDG_app.py
@@ -17,9 +17,7 @@ with tab1:
     st.write(
     "The Sustainable Development Report (SDR) reviews progress made each year on the Sustainable Development Goals "
     "since their adoption by the 193 UN Member States in 2015. At the halfway mark to 2030, the Sustainable Development "
-    "Report 2023 takes stock of progress made and discusses priorities to restore and accelerate SDG progress. Published "
-    "on the eve of the 2023 Paris Summit for a New Global Financial Pact, this year’s edition focuses specifically on the "
-    "need to scale up development finance and reform the global financial architecture to support the SDGs."
+    "Report 2023 takes stock of progress made and discusses priorities to restore and accelerate SDG progress."
 )
     st.markdown(
     "Database retrieved from Kaggle: "
