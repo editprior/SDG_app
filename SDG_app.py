18012ff314123e47091f1902b6c787079e04df39
editprior <146826298+editprior@users.noreply.github.com>
2024-03-12 14:51:16 +0100

    Update SDG_app.py

--- SDG_app.py
+++ SDG_app.py
@@ -13,7 +13,7 @@ df = pd.read_csv('sdg_index_2000-2022.csv')
 tab1, tab2, tab3 = st.tabs(['Overview', 'France and Ranking', 'Worldwide'])
 
 with tab1:
-    st.markdown("<h2>Sustainable Development Report</h2>", unsafe_allow_html=True)
+    st.markdown("<h2>Sustainable Development Report 2000-2022</h2>", unsafe_allow_html=True)
     st.write(
     "The Sustainable Development Report (SDR) reviews progress made each year on the Sustainable Development Goals "
     "since their adoption by the 193 UN Member States in 2015. At the halfway mark to 2030, the Sustainable Development "
